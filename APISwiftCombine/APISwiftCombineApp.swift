1acd7f0906fb21b193bb0cc5fa7a32540361fbae
Tibirica Neto <tibirabotto@Tibiricas-MacBook-Pro.local>
2022-11-04 16:40:57 -0400

    first commit

---
+++ APISwiftCombine/APISwiftCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  APISwiftCombineApp.swift
+//  APISwiftCombine
+//
+//  Created by Tibirica Neto on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct APISwiftCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APISwiftCombine/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  APISwiftCombine
+//
+//  Created by Tibirica Neto on 2022-11-04.
+//
+
+import SwiftUI
+import Combine
+
+struct PostModel: Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class DownloadWithCombineViewModel: ObservableObject {
+    @Published var posts: [PostModel] = []
+    var cancellables = Set<AnyCancellable>()
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return}
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            //.subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap(handleOutPut)
+            .decode(type: [PostModel].self, decoder: JSONDecoder())
+            .replaceError(with: [])
+            .sink(receiveValue: { [weak self] (returnedPosts) in
+                self?.posts = returnedPosts
+            })
+            .store(in: &cancellables)
+    }
+    
+    func handleOutPut(output: URLSession.DataTaskPublisher.Output ) throws -> Data {
+        guard
+            let response = output.response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
+}
+
+struct ContentView: View {
+    
+    @StateObject var vm = DownloadWithCombineViewModel()
+    
+    var body: some View {
+        List() {
+            ForEach(vm.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
